778f4ed899b76c362e21b62541095746c349dd62
LexTruong <lextruong777@gmail.com>
2023-11-15 20:13:29 -0800

    arc business

--- webjam/index.js
+++ webjam/index.js
@@ -1,4 +1,23 @@
-function gotoLink(link){
-    console.log(link.value);
-    location.href = 'http://127.0.0.1:5500/bus.html';
-};
+var curTime = new Date();
+var curHour = curTime.getHours();
+var curMins = curTime.getMinutes();
+var curDay = curTime.getDay();
+var AM = "AM";
+if(curHour > 12) AM = "PM";
+if(curMins < 10) curMins = "0" + curMins;
+
+document.getElementById("time").innerHTML = `The time is ${curHour%12}:${curMins} ${AM} and the ARC is currently`;
+
+if(curDay == 6 && (curHour < 8 || curHour > 21)) {
+    document.getElementById("answer").innerHTML = "CLOSED";
+} else if(curDay == 0 && (curHour < 8 )) {
+    document.getElementById("answer").innerHTML = "CLOSED";
+} else if(curHour < 6) {
+    document.getElementById("answer").innerHTML = "CLOSED";
+} else if(curHour < 8 || curHour > 20) {
+    document.getElementById("answer").innerHTML = "NOT BUSY";
+} else if (curHour < 2 || curHour > 4) {
+    document.getElementById("answer").innerHTML = "BUSY";
+} else {
+    document.getElementById("answer").innerHTML = "SOMEWHAT BUSY";
+}
